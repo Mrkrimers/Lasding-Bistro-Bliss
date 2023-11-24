bd60abdb640f0f0b6410cbc462046aa239aedc4c
mrkrimers <mrkrimerssasha@gmail.com>
2023-11-24 23:22:35 +0300

    added fonts

--- client/src/app/layout.js
+++ client/src/app/layout.js
@@ -2,6 +2,7 @@ import { Inter } from 'next/font/google'
 import './globals.scss'
 import Header from '@/components/Header/Header'
 import Footer from '@/components/Footer/Footer'
+import Head from 'next/head'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -13,6 +14,11 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link href="https://fonts.googleapis.com/css2?family=DM+Sans:opsz,wght@9..40,400;9..40,500;9..40,600;9..40,700&family=Playfair+Display:ital,wght@0,400;0,500;0,600;1,600&display=swap" rel="stylesheet" />
+      </Head>
       <body className={inter.className}>
         <Header />
 
